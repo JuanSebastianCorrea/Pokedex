fd033e9c79d51ecfe5233b7f2b0d0f085e79077b
Juan Sebastian Correa <jcossa1989@gmail.com>
2021-04-28 13:09:12 -0500

    initial commit

---
+++ src/Pokecard.js
@@ -0,0 +1,17 @@
+import './Pokecard.css';
+
+const POKE_API = 'https://raw.githubusercontent.com/' + 'PokeAPI/sprites/master/sprites/pokemon/';
+
+const Pokecard = (props) => {
+	let imgSrc = `${POKE_API}${props.id}.png`;
+	return (
+		<div className="Pokecard">
+			<h3 className="Pokecard-name">{props.name}</h3>
+			<img src={imgSrc} className="Pokecard-img" alt="" />
+			<p>Type: {props.type}</p>
+			<p>EXP: {props.base_experience}</p>
+		</div>
+	);
+};
+
+export default Pokecard;
---
+++ src/Pokedex.js
@@ -0,0 +1,17 @@
+import Pokecard from './Pokecard';
+import './Pokedex.css';
+
+const Pokedex = (props) => {
+	return (
+		<div className="Pokedex">
+			<h1 className="Pokedex-header">Pokedex</h1>
+			<div className="Pokedex-cards">
+				{props.pokemon.map((p) => (
+					<Pokecard id={p.id} name={p.name} img={p.img} type={p.type} base_experience={p.base_experience} />
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default Pokedex;
